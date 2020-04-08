fe1b0cc25538d674884e2f763f71c84785e7f09f
Keith <keith.davey@slh.ie>
2020-04-08 12:40:53 +0100

    initial commit

---
+++ content/first_post.md
@@ -0,0 +1,8 @@
+Title: My First Post
+Author: Keith Davey
+Date: 2020-04-03 11:40
+Category: Portfolio Post
+Tags: python, markdown, pelican
+Slug: my-first-post
+
+The article content goes here
---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Keith Davey'
+SITENAME = 'Keith\'s Website'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Dublin'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Ars Technica', 'https://arstechnica.com/'),
+     ('Wikipedia', 'https://wikipedia.org'),)
+
+SOCIAL = (('Twitter', 'https://twitter.com/mbdevaney'),
+         ('Github', 'https://github.com/yourekittenme'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+OUTPUT_PATH = '../output'
+THEME = 'theme'
+PLUGIN_PATHS = ['plugins/', ]
+PLUGINS = ['i18n_subsites', ]
+JINJA_ENVIRONMENT = {
+    'extensions': ['jinja2.ext.i18n'],
+}
+
+BOOTSTRAP_THEME = 'flatly'
+PYGMENTS_STYLE = 'monokai'
